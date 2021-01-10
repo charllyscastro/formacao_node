4dc230050c771505d11c1ac97e64c9fdc2523ed3
Charllys Castro <charllyscastro@gmail.com>
2021-01-10 20:43:11 -0300

    Carregamento automatico

--- aula3-express/index.js
+++ aula3-express/index.js
@@ -1,6 +1,10 @@
 const express = require('express'); //Carrega os modulos do express
 const app = express(); //Instancia o express
 
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
 app.listen(3000, (error) => {
   if(error){
     console.log('Servidor não foi iniciado.')
