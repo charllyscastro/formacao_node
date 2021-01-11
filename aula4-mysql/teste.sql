31d3cf84a401bd8429d8ace92ea135fd3f1ec266
Charllys Castro <charllyscastro@gmail.com>
2021-01-11 11:11:27 -0300

    insert, select e where

--- aula4-mysql/teste.sql
+++ aula4-mysql/teste.sql
@@ -3,3 +3,17 @@ CREATE TABLE usuarios(
   email VARCHAR(100),
   idade INT
 );
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+  "Fulano de Tal",
+  "email@email.com",
+  18
+);
+
+-- SHOW DATABASES - Mostra todos os bancos
+-- CREATE DATABASE nome_banco - Cria banco
+-- USE nome_banco - Acessa o banco
+-- CREATE TABLE nome_tabela - Cria a tabela
+-- DESCRIBE nome_tabela - Ver estrutura da tabela
+-- INSERT INTO nome_banco() VALUES() - Inseri dados na tabela
+-- SELECT * FROM nome_tabela - Lista todos os dados da tabela
