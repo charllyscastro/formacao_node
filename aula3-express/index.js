548a4ead4c1a7b3bbcf6109d59c115ea43d64526
Charllys Castro <charllyscastro@gmail.com>
2021-01-10 21:16:46 -0300

    Parâmetros

--- aula3-express/index.js
+++ aula3-express/index.js
@@ -5,6 +5,19 @@ app.get('/', (req, res) => {
   res.send('Home');
 });
 
+app.get('/ola/:nome/:empresa', (req, res) => {
+  const nome = req.params.nome;
+  const empresa = req.params.empresa;
+  res.send(`Oi! ${nome} ${empresa}`);
+});
+
+// parâmetro não obrigatorio
+app.get('/blog/:artigo?', (req, res) => {
+  const artigo = req.params.artigo;
+  artigo ? res.send(`Artigo: ${artigo}`) : res.send(`Artigo: Parâmetro não obrigatório.`)
+  
+});
+
 app.listen(3000, (error) => {
   if(error){
     console.log('Servidor não foi iniciado.')
