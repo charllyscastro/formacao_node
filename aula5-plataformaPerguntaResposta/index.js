cb3b93dcea4107d91c898c332ec3efe57d976024
Charllys Castro <charllyscastro@gmail.com>
2021-01-11 15:44:07 -0300

    Estruturas condicionais com EJS

--- aula5-plataformaPerguntaResposta/index.js
+++ aula5-plataformaPerguntaResposta/index.js
@@ -7,9 +7,11 @@ app.set('view engine', 'ejs');
 app.get('/:nome?/:lang?', (req, res) => {
   const nome = req.params.nome;
   const lang = req.params.lang;
+  const msg = false;
   res.render('index',{
     nome: nome, 
-    lang: lang
+    lang: lang,
+    msg
   });
 });
 
