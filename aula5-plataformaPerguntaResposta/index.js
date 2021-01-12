78bffdc87d6b646531f04017f648f68114ddeb89
Charllys Castro <charllyscastro@gmail.com>
2021-01-11 21:14:55 -0300

    Capturando dados de um formulário

--- aula5-plataformaPerguntaResposta/index.js
+++ aula5-plataformaPerguntaResposta/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Usando o ejs para usar como view engine
 app.set('view engine', 'ejs');
 // Utilizando arquivos estaticos
 app.use(express.static('public'));
+// Usando bodyParser para decodificar os dados do formulário
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); // Usando json
 
+//Rotas
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -15,7 +20,9 @@ app.get('/perguntar', (req, res) => {
 });
 
 app.post('/salvarpergunta',(req, res) => {
-
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+  res.send(`${titulo} ${descricao}`)
 });
 
 app.listen(3000, () => {
