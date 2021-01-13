b91e20c3516a367060d38f9e5b6fc7fd40802426
Charllys Castro <charllyscastro@gmail.com>
2021-01-13 19:03:13 -0300

    Configuração .ENV

--- aula5-plataformaPerguntaResposta/database/database.js
+++ aula5-plataformaPerguntaResposta/database/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
+require('dotenv/config');
 
-const connection = new Sequelize('guiaperguntas', 'root', 'mysql',{
-  host: 'localhost',
+const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 });
 
