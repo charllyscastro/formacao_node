890f0d60fcdb9dce05de7d00e35b489087311e9c
Charllys Castro <charllyscastro@gmail.com>
2021-01-16 19:29:24 -0300

    Correção paginação

--- aula7-guiapress/articles/ArticlesController.js
+++ aula7-guiapress/articles/ArticlesController.js
@@ -110,7 +110,7 @@ router.get('/articles/page/:num', (req, res) => {
   if(isNaN(page) || page == 1){
     offset =0;
   }else{
-    offset = parseInt(page)*2;
+    offset = (parseInt(page)-1)*4;
   }
 
   Article.findAndCountAll({
