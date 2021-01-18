9f77a50e56aec6425576545182013a762c5ab6dc
Charllys Castro <charllyscastro@gmail.com>
2021-01-17 23:36:45 -0300

    Hash da senha

--- aula7-guiapress/users/UsersController.js
+++ aula7-guiapress/users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Users = require('./Users');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
   res.send('Listagem de usuarios');
@@ -13,7 +14,18 @@ router.get('/admin/users/create', (req, res) => {
 router.post('/users/create', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  res.json({email, password})
+
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  Users.create({
+    email: email,
+    password: hash
+  }).then(() => {
+    res.redirect('/');
+  }).catch((err) => {
+    res.redirect('/');
+  });
+ 
 });
 
 module.exports = router;
