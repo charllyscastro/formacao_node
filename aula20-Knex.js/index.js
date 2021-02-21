a72c38b06b13ba296ea1771220e8d1d5612ee030
Charllys <charllyscastro@gmail.com>
2021-02-21 18:51:08 -0300

    Relacionamento de 1 para N

--- aula20-Knex.js/index.js
+++ aula20-Knex.js/index.js
@@ -110,3 +110,25 @@ database.select(["games.*", "estudios.nome as estudio_nome"])
           console.log(err);
       })
 */
+/*
+database.select(["games.*", "estudios.nome as estudio_nome"]).table("games").innerJoin("estudios","estudios.game_id","games.id")
+  .then(data => {
+    let game = {
+      id: 0,
+      nome: "",
+      estudios: []
+    }
+
+    game.id = data[0].id;
+    game.nome = data[0].nome;
+
+    data.forEach(estudio => {
+     game.estudios.push({nome: estudio.estudio_nome});
+    })
+
+    console.log(game);
+    
+}).catch(err => {
+    console.log(err);
+})
+*/
