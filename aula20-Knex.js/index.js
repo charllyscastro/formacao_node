0aa43b5e31d4c76962ff64b9ee65fec3299f729e
Charllys <charllyscastro@gmail.com>
2021-02-20 21:59:50 -0300

    Select e Nested queries

--- aula20-Knex.js/index.js
+++ aula20-Knex.js/index.js
@@ -7,10 +7,38 @@ const dados = [
   }
 ]
 
-/** Insert
+/** 
+ * ---Insert---
  database.insert(dados).into('games').then(data => {
    console.log(data);
  }).catch(err => {
    console.log(err);
  });
  */
+/*
+//  ---Select---
+async function index(){
+  try{
+    const resp =  await database.select(["nome", "preco"]).table("games")
+    console.log(resp);
+  }catch(err) {
+    console.log(err);
+  }
+}
+index();
+*/
+ 
+/*
+---Nested queries
+database.insert({nome: "Mists", preco: 25}).into('games').then(data => {
+  database.select(["nome", "preco"]).table("games").then(data => {
+    console.log(data);
+   }).catch(err => {
+    console.log(err);
+   });
+}).catch(err => {
+  console.log(err);
+});
+*/
+
+
