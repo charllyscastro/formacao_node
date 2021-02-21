4b47dca35216010537dad2146513f8f0cb147d5b
Charllys <charllyscastro@gmail.com>
2021-02-21 15:32:20 -0300

    Relacionamento 1 para 1

--- aula20-Knex.js/index.js
+++ aula20-Knex.js/index.js
@@ -91,10 +91,11 @@ database.insert({
   console.log(err);
 })
 */
-
+/*Relacionamento 1 para 1
 database.select(["games.*", "estudios.nome as estudio_nome"]).table("games").innerJoin("estudios","estudios.game_id","games.id")
   .then(data => {
     console.log(data);
 }).catch(err => {
     console.log(err);
 })
+*/
