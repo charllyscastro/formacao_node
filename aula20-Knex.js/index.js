683f8e98cdf32f7ad68a431cfd354ab6a83a13ef
Charllys <charllyscastro@gmail.com>
2021-02-21 15:29:50 -0300

    Relacionamento 1 para 1

--- aula20-Knex.js/index.js
+++ aula20-Knex.js/index.js
@@ -91,3 +91,10 @@ database.insert({
   console.log(err);
 })
 */
+
+database.select(["games.*", "estudios.nome as estudio_nome"]).table("games").innerJoin("estudios","estudios.game_id","games.id")
+  .then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+})
