9844e2146cc331ddfe7c3ebe5f69d6d7d4f758fa
Charllys <charllyscastro@gmail.com>
2021-02-20 21:35:03 -0300

    Knex Insert

--- aula20-Knex.js/index.js
+++ aula20-Knex.js/index.js
@@ -1,2 +1,16 @@
 const database = require('./database');
 
+const dados = [
+  {
+    nome: 'Call of duty',
+    preco: 50.56
+  }
+]
+
+/** Insert
+ database.insert(dados).into('games').then(data => {
+   console.log(data);
+ }).catch(err => {
+   console.log(err);
+ });
+ */
