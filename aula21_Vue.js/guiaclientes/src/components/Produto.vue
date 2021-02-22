d389da634362cc8880bf9084b70efa99fe92e0d4
Charllys <charllyscastro@gmail.com>
2021-02-21 23:44:36 -0300

    Criação de componentes

---
+++ aula21_Vue.js/guiaclientes/src/components/Produto.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="produto">
+    <h2>Produtos</h2>
+    <input type="text" placeholder="Insira seu produto">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  #produto{
+    color: blue;
+    width: 100%;
+    display:flex;
+    align-items: center;
+    justify-content: left;
+  }
+  h2{
+    margin-right: 5px;
+  }
+</style>
